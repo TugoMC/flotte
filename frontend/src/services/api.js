c84e25193a3e35bf5b99d6bc4fccc6bc75654004
Ouattara Junior <ouattarajunior418@gmail.com>
2025-05-12 16:54:37 +0000

    correction date-fns

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { toast } from 'sonner';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL: API_URL
